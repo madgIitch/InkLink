3ff5a6ebd3cab2e14e9d4d50c48e3af719591bf2
Pepe Ortiz Roldán <99665744+madgIitch@users.noreply.github.com>
2025-10-07 17:07:20 +0200

    Configurar la app para funcionar como PWA instalable en dispositivos móviles.

---
+++ app/sw-registrar.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { useEffect } from "react";
+import { registerSW } from "@/lib/sw-register";
+
+
+export default function SWRegistrar() {
+useEffect(() => {
+registerSW();
+}, []);
+return null;
+}
